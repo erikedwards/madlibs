c322123e797eed25acc2f798561af3743b1163fb
erik <erik.p.edwards@gmail.com>
2019-10-05 12:33:21 -0700

    first commit

---
+++ libz.py
@@ -0,0 +1,20 @@
+# mad libz
+# 10-05-2019
+
+#
+# take me _(direction)_
+# to the Paradise City
+# where the _(noun)_ is _(color)_
+# and the _(plural nouns)_ are _(adjective what rhymes with "City")_
+#
+
+fun = True
+
+while fun:
+    direction = input("Enter a direction in which one could be taken")
+    noun = input("Enter a noun")
+    color = input("Enter a color")
+    plural_noun = input("Enter a plural noun")
+    adj_rhyme = input("Enter an adjective that rhymes with 'City', like maybe 'gritty' or 'shitty' or even 'witty'.")
+
+    print("take me down to the paradise")
