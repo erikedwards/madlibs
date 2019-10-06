b0827c6eec61210b72e7631fbaa773d88ed4fea1
erikedwards <erik.p.edwards@gmail.com>
2019-10-05 23:32:13 -0700

    works now

--- libz.py
+++ libz.py
@@ -6,7 +6,7 @@
 # to the Paradise City
 # where the _(noun)_ is _(color)_
 # and the _(plural nouns)_ are _(adjective what rhymes with "City")_
-#
+#down
 
 fun = True
 
@@ -17,4 +17,13 @@ while fun:
     plural_noun = input("Enter a plural noun")
     adj_rhyme = input("Enter an adjective that rhymes with 'City', like maybe 'gritty' or 'shitty' or even 'witty'.")
 
-    print("take me down to the paradise")
+    print("take me down {}\n"
+          "to the paradise city\n"
+          "where the grass {} is green {} \n"
+          "and the girls {} are pretty {}\n"
+          "OH WON'T YOU PLEASE\n"
+          " TAKE ME HOME... WOH-OH-OH-AH".format(direction, noun, color, plural_noun, adj_rhyme))
+
+    ans = input("again? (y/n)")
+    if ans == "n":
+        fun = False
